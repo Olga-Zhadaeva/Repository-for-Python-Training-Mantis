c7164f6ace7874da1ca4bba9f7356c4a7cc61f4e
olgazhadaeva <olga.zhadaeva14@gmail.com>
2025-05-09 01:53:14 +0300

    Основа адреса веб-сервиса взята из конфигурационного файла target.json

--- fixture/soap.py
+++ fixture/soap.py
@@ -8,7 +8,7 @@ class SoapHelper:
         self.app = app
 
     def get_project_list(self, username, password):
-        client = Client('http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl')
+        client = Client(f"{self.app.base_url}api/soap/mantisconnect.php?wsdl")
         try:
             project_list = client.service.mc_projects_get_user_accessible(username, password)
             return list(project_list)
